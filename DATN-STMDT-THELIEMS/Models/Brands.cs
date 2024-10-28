60e584e7af476a0513d5e33c4fb06a7f5ef3298e
AnhChuongChovy <chuonggo51@gmail.com>
2024-10-28 19:50:13 +0700

    Đổ dữ liệu lên giao diện QL sản phẩm shop (FE)

--- DATN-STMDT-THELIEMS/Models/Brands.cs
+++ DATN-STMDT-THELIEMS/Models/Brands.cs
@@ -6,9 +6,9 @@
         public string Name { get; set; } // nvarchar(50)
         public string Description { get; set; } // Text
         public string Image { get; set; } // Text
-        public byte Status { get; set; } // Tinyint -> bool
-        public DateTime Created_at { get; set; }
-        public DateTime Updated_at { get; set; }
+        public bool Status { get; set; } // Tinyint -> bool
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
         public ICollection<Products> Products { get; set; }
 
     }
