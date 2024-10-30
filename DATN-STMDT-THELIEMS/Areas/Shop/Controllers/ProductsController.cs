c60efa33d543c74126f20a5968626636bd12ed04
HuynhQuyTruong <truonghqpc05828@fpt.edu.vn>
2024-10-30 21:41:55 +0700

    up lại

---
+++ DATN-STMDT-THELIEMS/Areas/Shop/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using DATN_STMDT_THELIEMS.DATA;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATN_STMDT_THELIEMS.Areas.Shop.Controllers
+{
+    [Area("Shop")]
+    [Route("Shop/[controller]/[action]")]
+    public class ProductsController : Controller
+    {
+		private readonly AppDBContext _context;
+
+		public ProductsController(AppDBContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult IndexShop()
+        {
+			var products = _context.PRODUCTS
+							  
+							  .ToList();
+
+			// Truyền dữ liệu vào View
+			return View(products);
+		}
+
+		public IActionResult AddProduct() 
+		{
+			return View();
+		}
+    }
+}
