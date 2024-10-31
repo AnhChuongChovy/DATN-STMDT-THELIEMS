a5cf62d5f98efacf277d088719b7ff6dc806b94c
AnhChuongChovy <chuonggo51@gmail.com>
2024-10-31 16:57:42 +0700

    Đổ dữ liệu List user (FE), đổ dữ liệu hoá đơn (FE)

--- DATN-STMDT-THELIEMS/Models/Variant_options.cs
+++ DATN-STMDT-THELIEMS/Models/Variant_options.cs
@@ -3,8 +3,8 @@
     public class Variant_options
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Variant_values> Variant_values { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Variant_values>? Variant_values { get; set; }
 
     }
 }
